460e18b71c8ec10d028dbe2d530f1292caa82060
Joced Nieves <joced.alex@gmail.com>
2022-03-27 14:30:51 -0600

    Dynamic Item showing

--- Store/views.py
+++ Store/views.py
@@ -7,3 +7,9 @@ from .models import Item
 class itemsView(ListView):
     model=Item
     template_name='store.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title']='Django Store Joced Nieves'
+        return context
+        
